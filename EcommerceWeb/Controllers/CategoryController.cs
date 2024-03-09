638bfdfb5a76b73eff9079025daffe6d3139e725
azizulhakim13 <mah.azizulhakim.sust@gmail.com>
2024-03-09 17:51:34 +0600

    form to add category with tag helpers

--- EcommerceWeb/Controllers/CategoryController.cs
+++ EcommerceWeb/Controllers/CategoryController.cs
@@ -16,5 +16,10 @@ namespace EcommerceWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
