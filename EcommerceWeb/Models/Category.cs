14e9861441c901b5f31f9288c65e87726f3154dc
azizulhakim13 <mah.azizulhakim.sust@gmail.com>
2024-03-10 12:22:35 +0600

    server side validation

--- EcommerceWeb/Models/Category.cs
+++ EcommerceWeb/Models/Category.cs
@@ -8,9 +8,11 @@ namespace EcommerceWeb.Models
         [Key]
         public int CategoryId { get; set; }
         [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100.\r\n \r\n")]
         public int DisplayOrder { get; set; }
     }
 }
